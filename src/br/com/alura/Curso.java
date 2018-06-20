697e1798295c0f62776ee311389d7516eca2a2ef
rodrigoapolinaro <rodrigoapolinaro@icloud.com>
2018-06-20 10:45:00 -0300

    Aula04 - Atualizando Mais práticas com relacionamentos

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -5,11 +5,11 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Curso {
-	
+
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
-	
+
 	public Curso(String nome, String instrutor) {
 		this.nome = nome;
 		this.instrutor = instrutor;
@@ -18,17 +18,27 @@ public class Curso {
 	public String getNome() {
 		return nome;
 	}
-	
+
 	public String getInstrutor() {
 		return instrutor;
 	}
-	
+
 	public List<Aula> getAulas() {
 		return Collections.unmodifiableList(aulas);
 	}
-	
+
 	public void adiciona(Aula aula) {
 		this.aulas.add(aula);
 	}
-	
+
+	public int getTempoTotal() {
+		return this.aulas.stream().mapToInt(Aula::getTempo).sum();
+	}
+
+	@Override
+	public String toString() {
+		return "[Curso: " + this.nome + ", tempo total: " + this.getTempoTotal() 
+				+ ", " + "aulas: " + this.aulas + "]";
+	}
+
 }
